bd0dca885f08b8182c529cd472f0ee2b08bc7bb5
demattosanthony <demattosanthony@gmail.com>
2022-05-19 21:40:32 -0400

    link issue

--- next-app/pages/api/blockfrost/accounts/[stake_address]/addresses/assets.js
+++ next-app/pages/api/blockfrost/accounts/[stake_address]/addresses/assets.js
@@ -2,7 +2,6 @@ import client from '../../../auth.js'
 
 export default async function handler(req, res) {
   const { stake_address } = req.query
-  console.log(stake_address)
   try {
     const response = await client.accountsAddressesAssets(stake_address)
     res.status(200).json(response)
